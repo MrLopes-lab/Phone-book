32f5bc9f675f3ff9e3c2e737f73997a221d91a0e
Marcelo Lopes Rodrigues <lopesrodrigues05@gmail.com>
2020-02-10 15:31:46 -0300

    Model e Controller

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 import { Router } from 'express';
 
+import UserControler from './app/controller/UserController';
+
 const routes = new Router();
+
+routes.post('/users', UserControler.store);
 
 export default routes;
